6d610fa76b019f2b59798852028e74d89ccf470b
Vadym Abakumov <vadym.abakumov@gmail.com>
2026-03-28 17:25:35 +0100

    fix: removed NEXT_PUBLIC prefix

--- apps/client/src/lib/api.ts
+++ apps/client/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_SERVER_URL ?? 'http://localhost:3001',
+	baseURL: process.env.SERVER_URL ?? 'http://localhost:3001',
 });
 
 export default api;
