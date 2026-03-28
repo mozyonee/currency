21063ba1401b68e5fb10c823f77fd17ea42d5c2f
Vadym Abakumov <vadym.abakumov@gmail.com>
2026-03-28 14:57:22 +0100

    fix: imported config module to server

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CurrencyModule } from './currency/currency.module.js';
 
 @Module({
-	imports: [CurrencyModule],
+	imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env.local', '.env'] }), CurrencyModule],
 	controllers: [],
 	providers: [],
 })
